6fe77444136f4561433d7aee96d127974b0fd32c
Eric Mauldin <emauldin84@gmail.com>
2020-04-03 15:34:49 -0400

    adds buttons to navigate between the home and reviewdetails screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {StyleSheet, View, Text } from 'react-native'
+import {StyleSheet, View, Text, Button } from 'react-native'
 
 import { globalStyles } from '../styles/global'
 
-const Home = () => {
+const Home = ( { navigation }) => {
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+        // or...
+        // navigation.push('ReviewDetails')
+    }
+    
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title='review details' onPress={pressHandler}/>
         </View>
     )
 }
--- screens/ReviewsDetails.js
+++ screens/ReviewsDetails.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import {StyleSheet, View, Text } from 'react-native'
+import {StyleSheet, View, Text, Button } from 'react-native'
 
 import { globalStyles } from '../styles/global'
 
 
-const ReviewDetails = () => {
+const ReviewDetails = ({ navigation }) => {
+
+    const pressHandler = () => {
+        navigation.goBack()
+        // or...
+        // navigation.pop()
+    }
+    
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            <Button title='back to home' onPress={pressHandler}/>
         </View>
     )
 }
