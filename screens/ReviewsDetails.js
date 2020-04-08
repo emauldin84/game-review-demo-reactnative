4c019e1ca305055e83c6570783a211f74a9f1e53
Eric Mauldin <emauldin84@gmail.com>
2020-04-08 15:01:14 -0400

    adds images object to global file and dynamically displays appropriate image based on game rating

--- screens/ReviewsDetails.js
+++ screens/ReviewsDetails.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {StyleSheet, View, Text } from 'react-native'
+import {StyleSheet, View, Text, Image } from 'react-native'
 
-import { globalStyles } from '../styles/global'
+import { globalStyles, images } from '../styles/global'
+
 import Card from '../shared/Card'
 
 
@@ -12,16 +13,33 @@ const ReviewDetails = ({navigation, route}) => {
         // navigation.pop()
     // }
 
+    const rating = route.params.rating
+
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{route.params.title}</Text>
                 <Text>{route.params.body}</Text>
                 <Text>{route.params.rating}</Text>
+                <View style={styles.rating}>
+                    <Text>Rating: </Text>
+                    <Image source={images.ratings[rating]}/>
+                </View>
             </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+})
 
 
 export default ReviewDetails
