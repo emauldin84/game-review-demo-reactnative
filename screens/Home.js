ec09822451b4cb41d0af4d8296d9d5e46e77369c
Eric Mauldin <emauldin84@gmail.com>
2020-04-13 11:31:22 -0400

    submits new review form data to state, clears the fields, closes the modal and allows keyboard to close when clicking away for the form.

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native'
+import {StyleSheet, View, Text, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import { globalStyles } from '../styles/global'
@@ -13,7 +13,6 @@ const Home = ( { navigation }) => {
         { title: 'Secret of Mana', rating: 3, body: 'lorem ipsum', key: '2'},
         { title: 'Final Fantasy VII', rating: 5, body: 'lorem ipsum', key: '3'},
     ])
-
     const [modalOpen, setModalOpen] = useState(false)
 
     // const pressHandler = () => {
@@ -22,18 +21,29 @@ const Home = ( { navigation }) => {
         // navigation.push('ReviewDetails')
     // }
 
+    const addReview = (review) => {
+        // lazy key creation...
+        review.key = Math.random().toString()
+        setReviews((currentReviews) => {
+            return [review, ...currentReviews]
+        })
+        setModalOpen(false)
+    }
+
     return (
         <View style={globalStyles.container}>
             <Modal visible={modalOpen} animationType='slide'>
-                <View style={styles.modalContent}>
-                    <MaterialIcons 
-                        name='close' 
-                        size={24}
-                        style={{...styles.modalToggle, ...styles.modalClose}} 
-                        onPress={() => setModalOpen(false)}
-                    />
-                    <ReviewForm />
-                </View>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.modalContent}>
+                        <MaterialIcons 
+                            name='close' 
+                            size={24}
+                            style={{...styles.modalToggle, ...styles.modalClose}} 
+                            onPress={() => setModalOpen(false)}
+                        />
+                        <ReviewForm addReview={addReview}/>
+                    </View>
+                </TouchableWithoutFeedback>
             </Modal>
             <MaterialIcons 
                 name='add' 
